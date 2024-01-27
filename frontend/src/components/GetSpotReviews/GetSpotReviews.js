884c15438107599f978b3bbc57446ad839bb02c0
ruokmain <anthony.hoang1991@gmail.com>
2024-01-26 18:52:05 -0800

    wip 9

--- frontend/src/components/GetSpotReviews/GetSpotReviews.js
+++ frontend/src/components/GetSpotReviews/GetSpotReviews.js
@@ -4,18 +4,31 @@ import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { NavLink } from "react-router-dom"
 import { fetchGetSpots } from '../../store/spots'
+import * as sessionActions from '../../store/session';
+import OpenModalMenuItem from '../Navigation/OpenModalMenuItem'
+import LoginFormModal from '../LoginFormModal'
+import SignupFormModal from '../SignupFormModal'
 
 const GetSpotReviews = ({ session, spot, reviews }) => {
-  console.log("SESSION: ", session)
-  console.log("SPOT: ", spot)
-  console.log("REVIEWS: ", reviews)
   const reviewArr = Object.values(reviews).reverse() || []
-  console.log("REVIEWSARRRR: ", reviewArr)
-  const checkUser = reviewArr.find((obj) => obj.userId == session.user.id)
+  const [showMenu, setShowMenu] = useState(false)
+  // checks if user has submitted a review
+  const checkUser = session.user ? reviewArr.find((obj) => obj.userId == session.user.id) : false
 
-  console.log(checkUser)
+  // checks if user owns the spot
+  const checkOwner = (session.user && session.user.id == spot.ownerId) ? true : false
 
   const monthNames = ['January', 'February', 'March,', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  const dispatch = useDispatch()
+
+  const closeMenu = () => setShowMenu(false);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+    closeMenu();
+  };
+
 
   return (
     <div>
@@ -29,9 +42,22 @@ const GetSpotReviews = ({ session, spot, reviews }) => {
         {!spot.avgRating ? <span>New</span> :
           <span> {spot.avgRating} &#x2022; {spot.numReviews > 1 ? <span>{spot.numReviews} reviews</span> : <span>{spot.numReviews} review</span>} </span>}
       </div>
+
+
+
+
       <div>
-          {session.user && !checkUser && <button>Post Your Review</button>}
+        {session.user && !checkUser && !checkOwner && <OpenModalMenuItem
+          itemText={<button>Post Your Review</button>}
+          onItemClick={closeMenu}
+          modalComponent={<LoginFormModal />}
+        />}
+        
       </div>
+      {/* {session.user && !checkUser && !checkOwner &&} */}
+
+
+
       <div>
         {!reviewArr.length ? <span>Be the first to post a review!</span> :
           reviewArr.map((review) => {
---
+++ frontend/src/components/ReviewFormModal/ReviewFormModal.js
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch } from "react-redux";
+import { useModal } from "../../context/Modal";
